1fc11f212181707c758676a0d025e63f78e2316d
rukkiecodes <rukkiecodes@gmail.com>
2023-06-21 02:38:05 +0100

    setting up home

--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: () => import('@/layouts/website/Default.vue'),
     children: [
       {
         path: '',
         name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () => import('@/views/website/Home.vue'),
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    component: () => import('@/layouts/admin/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Admin.Home',
+        component: () => import('@/views/Home.vue'),
       },
     ],
   },
---
+++ src/views/website/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <Heading />
+</template>
+
+<script setup>
+import Heading from '@/components/website/Head.vue'
+</script>
+
+<style></style>
