aa1d892fccd8559f03d9df3f8ecb122f5dc0a8cf
rukkiecodes <rukkiecodes@gmail.com>
2023-06-22 20:15:04 +0100

    head not workiong

--- src/components/website/Head.vue
+++ src/components/website/Head.vue
@@ -3,92 +3,111 @@
         src="https://res.cloudinary.com/rukkiecodes/image/upload/v1687297065/Titi%20Artistry%20Gallery/theregisti-HSXIp58yPyI-unsplash_pjns2x.jpg"
         lazy-src="https://res.cloudinary.com/rukkiecodes/image/upload/v1687297065/Titi%20Artistry%20Gallery/theregisti-HSXIp58yPyI-unsplash_pjns2x.jpg"
         gradient="rgba(0,0,0,0.8), rgba(0,0,0,0.4)" cover width="100vw" height="93vh">
-        <canvas class="outlineText"></canvas>
+        <div ref="canvasContainer"></div>
     </v-img>
 </template>
 
-<script setup>
-import * as THREE from 'three'
+<script>
+import * as THREE from 'three';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js'
-import { onMounted } from "vue";
 
-onMounted(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('.outlineText') });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+export default {
+    mounted() {
+        // Set up the scene, camera, and renderer
+        this.init();
 
-    // Create the text geometry
-    const textGeometry = new TextGeometry('Hello, World!', {
-        font: 'helvetiker',
-        size: 50,
-        height: 5,
-        curveSegments: 12,
-        bevelEnabled: true,
-        bevelThickness: 2,
-        bevelSize: 1,
-        bevelOffset: 0,
-        bevelSegments: 5
-    });
+        // Load the font and create text geometry
+        const fontLoader = new FontLoader();
+        fontLoader.load('https://threejsfundamentals.org/threejs/resources/threejs/fonts/helvetiker_regular.typeface.json', (font) => {
+            const textGeometry = new TextGeometry('Hello', {
+                font: font,
+                size: 1,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelSegments: 5,
+            });
 
-    // Create the text material
-    const textMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+            // Create text material
+            const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
 
-    // Create the text mesh
-    const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-    scene.add(textMesh);
+            // Create text mesh
+            const textMesh = new THREE.Mesh(textGeometry, textMaterial);
 
-    // Create the outline material
-    const outlineMaterial = new THREE.ShaderMaterial({
-        uniforms: {
-            "c": { type: "f", value: 0.1 },
-            "p": { type: "f", value: 4.0 }
-        },
-        vertexShader: `
-        uniform float c;
-        uniform float p;
-        
-        varying vec2 vUv;
-        
-        void main() {
+            // Add text mesh to the scene
+            this.scene.add(textMesh);
+
+            // Create outline material
+            const outlineMaterial = new THREE.ShaderMaterial({
+                vertexShader: `
+          varying vec2 vUv;
+          void main() {
             vUv = uv;
-            vec4 pos = modelViewMatrix * vec4(position, 1.0);
-            float d = length(vec2(dFdx(pos.x), dFdy(pos.y)));
-            gl_Position = projectionMatrix * pos;
-            gl_Position.xy += normal.xy * c * d * sin(p * gl_Position.y);
-        }
-    `,
-        fragmentShader: `
-        varying vec2 vUv;
-        
-        void main() {
-            float d = length(vUv - vec2(0.5));
-            float alpha = smoothstep(0.45, 0.55, d);
-            gl_FragColor = vec4(0.0, 0.0, 0.0, alpha);
-        }
-    `,
-        side: THREE.BackSide,
-        blending: THREE.AdditiveBlending,
-        transparent: true
-    });
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+          }
+        `,
+                fragmentShader: `
+          varying vec2 vUv;
+          void main() {
+            float distance = length(vec2(0.5, 0.5) - vUv);
+            float alpha = smoothstep(0.45, 0.55, distance);
+            gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
+          }
+        `,
+                transparent: true,
+            });
 
-    // Create the outline mesh
-    const outlineMesh = new THREE.Mesh(textGeometry, outlineMaterial);
-    scene.add(outlineMesh);
+            // Create outline mesh
+            const outlineMesh = new THREE.Mesh(textGeometry, outlineMaterial);
+            outlineMesh.scale.set(1.1, 1.1, 1.1); // Slightly scale up the outline
 
-    // Position the camera
-    camera.position.z = 5;
+            // Add outline mesh to the scene
+            this.scene.add(outlineMesh);
+        });
 
-    // Animation loop
-    function animate() {
-        requestAnimationFrame(animate);
-        textMesh.rotation.x += 0.01;
-        textMesh.rotation.y += 0.01;
-        outlineMesh.rotation.x += 0.01;
-        outlineMesh.rotation.y += 0.01;
-        renderer.render(scene, camera);
-    }
+        // Start the animation loop
+        this.animate();
+    },
+    methods: {
+        init() {
+            // Create a scene
+            this.scene = new THREE.Scene();
 
-    animate();
-})
+            // Create a camera
+            this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+            this.camera.position.z = 5;
+
+            // Create a renderer
+            this.renderer = new THREE.WebGLRenderer({ antialias: true });
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+            // Append the renderer to the container element
+            this.$refs.canvasContainer.appendChild(this.renderer.domElement);
+        },
+        animate() {
+            requestAnimationFrame(this.animate);
+
+            // Rotate the text meshes
+            this.scene.traverse((object) => {
+                if (object instanceof THREE.Mesh) {
+                    object.rotation.x += 0.01;
+                    object.rotation.y += 0.01;
+                }
+            });
+
+            // Render the scene
+            this.renderer.render(this.scene, this.camera);
+        },
+    },
+};
 </script>
+
+
+<style scoped>
+canvas {
+    display: block;
+}
+</style>
