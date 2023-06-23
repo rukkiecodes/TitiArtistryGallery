0a8ac65f7bb60cada087e4e3d88c9dbb05d145fc
rukkiecodes <rukkiecodes@gmail.com>
2023-06-23 18:09:52 +0100

    restructure project structure

--- src/views/website/Home.vue
+++ src/views/website/Home.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import Heading from '@/components/website/Head.vue'
-import LatestWork from '@/components/website/LatestWork.vue'
-import About from '@/components/website/About.vue'
+import Heading from '@/components/website/home/Head.vue'
+import LatestWork from '@/components/website/home/LatestWork.vue'
+import About from '@/components/website/home/About.vue'
 </script>
