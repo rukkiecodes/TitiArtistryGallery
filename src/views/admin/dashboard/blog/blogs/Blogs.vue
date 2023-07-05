e6b551fa451c367b8eeef800f6e89e29f74dd22d
rukkiecodes <rukkiecodes@gmail.com>
2023-07-05 01:05:18 +0100

    add search to blog

--- src/views/admin/dashboard/blog/blogs/Blogs.vue
+++ src/views/admin/dashboard/blog/blogs/Blogs.vue
@@ -1,5 +1,20 @@
 <template>
   <v-container>
+    <v-toolbar color="amber" rounded="lg" class="mb-5 pr-4">
+      <v-btn to="/admin/dashboard/blog/newBlog" class="ma-5 bg-amber-darken-2 text-white text-capitalize" rounded="lg"
+        :icon="icon" height="45" elevation="0">
+        <span v-if="!icon">Write a new Blog</span>
+        <v-icon v-else>mdi-plus</v-icon>
+      </v-btn>
+
+      <v-text-field v-model="searchQuery" label="Search blogs" @input="updateSearch" hide-details density="compact"
+        color="white" variant="solo" rounded="lg" prepend-inner-icon="mdi-magnify" />
+    </v-toolbar>
+    <!-- <v-row>
+      <v-col cols="12">
+      </v-col>
+    </v-row> -->
+
     <v-row>
       <v-col cols="12" sm="4" md="4" v-for="blog in paginatedBlogs" :key="blog.id">
         <v-card rounded="lg" :to="`/admin/dashboard/blog/${blog.id}`">
@@ -20,36 +35,66 @@
       </v-col>
     </v-row>
 
-    <v-btn to="/admin/dashboard/blog/newBlog" position="absolute" location="bottom right"
-      class="ma-5 bg-amber-darken-2 text-white" rounded="lg" elevation="10">Write a new Blog</v-btn>
-
     <!-- Pagination component -->
-    <v-pagination v-model="currentPage" :length="totalPages" @input="changePage" />
+    <v-pagination v-model="currentPage" :length="totalPages" @input="changePage" class="mt-5" active-color="amber" />
   </v-container>
 </template>
   
 <script setup>
 import { useAdminBlogStore } from "@/store/admin/blog";
 import { ref, computed } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const { name } = useDisplay()
 
 const blog = useAdminBlogStore()
 
 const currentPage = ref(1);
 const itemsPerPage = 12; // Number of items per page
 
-
 // Calculate the total number of pages based on the number of blogs and itemsPerPage
-const totalPages = computed(() => Math.ceil(blog.blogs.length / itemsPerPage));
+const totalPages = computed(() => Math.ceil(filteredBlogs.length / itemsPerPage));
 
 // Calculate the index of the first and last item to be displayed on the current page
 const startIndex = computed(() => (currentPage.value - 1) * itemsPerPage);
 const endIndex = computed(() => startIndex.value + itemsPerPage);
 
+// Search functionality
+const searchQuery = ref('');
+const filteredBlogs = computed(() => {
+  const query = searchQuery.value.trim().toLowerCase();
+  if (query === '') return blog.blogs;
+  return blog.blogs.filter((blog) => {
+    return blog.title.toLowerCase().includes(query) ||
+      blog.title.toLowerCase().includes(query);
+  });
+});
+
 // Slice the blogs array based on the startIndex and endIndex
-const paginatedBlogs = computed(() => blog.blogs.slice(startIndex.value, endIndex.value));
+const paginatedBlogs = computed(() => filteredBlogs.value.slice(startIndex.value, endIndex.value));
 
 // Function to handle page change
 function changePage(page) {
   currentPage.value = page;
 }
+
+// Function to update search query
+function updateSearch() {
+  currentPage.value = 1; // Reset to first page when search query changes
+}
+
+const icon = computed(() => {
+  // name is reactive and
+  // must use .value
+  switch (name.value) {
+    case 'xs': return true
+    case 'sm': return false
+    case 'md': return false
+    case 'lg': return false
+    case 'xl': return false
+    case 'xxl': return false
+  }
+
+  return undefined
+})
 </script>
